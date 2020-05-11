1f6ee5bfebe7c6958b2f8700741f568690c72236
caogang <caogang_sx@qiyi.com>
2020-05-11 22:03:37 +0800

    最大正方形-leet221

---
+++ src/com/leet/array/MaximalSquare.java
@@ -0,0 +1,112 @@
+package com.leet.array;
+
+public class MaximalSquare {
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int row = matrix.length;
+        int column = matrix[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (matrix[i][j] == '1') {
+                    maxSide = Math.max(maxSide, 1);
+                    int currentMaxside = Math.min(row - i, column - j);
+                    for (int k = 1; k < currentMaxside; k++) {
+                        boolean flag = true;
+                        if (matrix[i + k][j + k] == '0') {
+                            break;
+                        }
+                        for (int m = 0; m < k; m++) {
+                            if (matrix[i + k][j + m] == '0' || matrix[i + m][j + k] == '0') {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if (flag) {
+                            maxSide = Math.max(maxSide, k + 1);
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return maxSide * maxSide;
+    }
+
+    public int maximalSquareDP(char[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int row = matrix.length;
+        int column = matrix[0].length;
+        // dp[i][j]表示以(i, j)为右下角的正方形的最长边长
+        int[][] dp = new int[row][column];
+        int maxSide = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (matrix[i][j] == '0') {
+                    dp[i][j] = 0;
+                } else {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                }
+                maxSide = Math.max(maxSide, dp[i][j]);
+            }
+        }
+        return maxSide * maxSide;
+    }
+
+    // 优化空间
+    // 每填一行只关心上一行，不再关心上上一行，即 「从上到下、逐行填写」
+    // 由此可得优化 dp 为 「只留最新的一行」，即 dp[column]
+    public int maximalSquareDPPlus(char[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int row = matrix.length;
+        int column = matrix[0].length;
+        int[] dp = new int[column];
+        int maxSide = 0;
+        // 保存左上角的值
+        int corner;
+        for (int i = 0; i < row; i++) {
+            corner = 0;
+            for (int j = 0; j < column; j++) {
+                // 注意上次的左上角是如何保存的
+                int lastCorner = dp[j];
+                if (matrix[i][j] == '0') {
+                    dp[j] = 0;
+                } else {
+                    if (i == 0 || j == 0) {
+                        dp[j] = 1;
+                    } else {
+                        // Math.min中的dp[j - 1]为当前行左边，dp[j]为上一行该列的值
+                        // 还差左上角的值（即之前的dp[i - 1][j - 1], 在计算dp[j - 1]的时候会被覆盖，所以单独保存）
+                        dp[j] = Math.min(Math.min(dp[j - 1], dp[j]), corner) + 1;
+                    }
+                }
+                corner = lastCorner;
+                maxSide = Math.max(maxSide, dp[j]);
+            }
+        }
+        return maxSide * maxSide;
+    }
+
+    public static void main(String[] args) {
+        MaximalSquare res = new MaximalSquare();
+        char[][] e = {{'1', '0', '1', '0'},
+                     {'1', '0', '1', '1'},
+                     {'1', '0', '1', '1'},
+                     {'1', '1', '1', '1'}};
+
+        System.out.println("------最大正方形面积： " + res.maximalSquare(e));
+        System.out.println("------最大正方形面积： " + res.maximalSquareDPPlus(e));
+    }
+
+}
---
+++ src/com/leet/array/MaximalSquare_failed.java
@@ -0,0 +1,106 @@
+package com.leet.array;
+
+public class MaximalSquare_failed {
+
+  // 记录向右和向下到达过的最大长度
+  private static int rightL = 1;
+  private static int downL = 1;
+  private static int sideL = 1;
+
+  public int maximalSquare(char[][] matrix) {
+
+    rightL = 1;
+    downL = 1;
+    sideL = 1;
+
+    if (matrix.length == 0 || matrix[0].length == 0) {
+      return 0;
+    }
+
+    boolean[][] isVisit = new boolean[matrix.length][matrix[0].length];
+
+    // 总的连通面积
+    int square = 0;
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        if (!isVisit[i][j]) {
+          square = Math.max(dfs(i, j, matrix, isVisit, 1, 1, 1, 1), square);
+        }
+      }
+    }
+
+    System.out.println("总面积：" + square);
+    System.out.println("向右最大深度：" + rightL);
+    System.out.println("向下最大深度：" + downL);
+    System.out.println("正方形边长最大深度：" + (sideL));
+
+    // 取向下和向右到达过的长度的较小值作为边
+    int side = Math.min(downL, rightL);
+
+    if (side * side > square) {
+      System.out.println("---该算法失败T^T---");
+      if (sideL < side && sideL == 1) {
+        return Math.min((sideL + 1) * (sideL + 1), square);
+      }
+      return Math.min((sideL) * (sideL), square);
+    }
+
+    return side * side;
+  }
+
+  private int dfs(int i, int j, char[][] matrix, boolean[][] isVisit,
+                  int rightlevel, int downLevel, int leftlevel, int upLevel) {
+    if (i >= 0 && i < matrix.length && j >= 0 && j < matrix[0].length && !isVisit[i][j] && matrix[i][j] == '1') {
+      isVisit[i][j] = true;
+
+      int right = dfs(i, j + 1, matrix, isVisit, rightlevel + 1, downLevel, leftlevel, upLevel);
+      int down = dfs(i + 1, j, matrix, isVisit, rightlevel, downLevel + 1, leftlevel, upLevel);
+      int left = dfs(i, j - 1, matrix, isVisit, rightlevel, downLevel, leftlevel + 1, upLevel);
+      int up = dfs(i - 1, j, matrix, isVisit, rightlevel, downLevel, leftlevel, upLevel + 1);
+
+      if (rightlevel == downLevel &&
+          downLevel == leftlevel) {
+        sideL = Math.max(rightlevel, sideL);
+      }
+      rightL = Math.max(rightlevel, rightL);
+      downL = Math.max(downLevel, downL);
+      return right + down + left + up + 1;
+    } else {
+      return 0;
+    }
+  }
+
+  public static void main(String[] args) {
+    MaximalSquare_failed res = new MaximalSquare_failed();
+    char[][] a = {{'1', '1', '0', '0', '0'},
+                  {'1', '1', '0', '0', '0'},
+                  {'0', '0', '1', '0', '0'},
+                  {'0', '0', '0', '1', '1'}};
+    char[][] b ={{'1'}};
+    char[][] c ={{'1', '1'},
+                  {'1', '1'}};
+
+    char[][] e = {{'0', '0', '0', '1'},
+                  {'1', '1', '0', '1'},
+                  {'1', '1', '1', '1'},
+                  {'0', '1', '1', '1'},
+                  {'0', '1', '1', '1'}};
+
+    char[][] g ={{'1', '1'}};
+
+    // 4
+    char[][] failed = {{'1', '0', '1', '0', '0'},
+                       {'1', '0', '1', '1', '1'},
+                       {'1', '1', '1', '1', '1'},
+                       {'1', '0', '0', '1', '0'}};
+
+    // 4
+    char[][] failed2 = {{'1', '1', '0', '1'},
+                        {'1', '1', '0', '1'},
+                        {'1', '1', '1', '1'}};
+
+
+//    System.out.println("------最大正方形面积： " + res.maximalSquare(a));
+    System.out.println("------最大正方形面积： " + res.maximalSquare(failed2));
+  }
+}
