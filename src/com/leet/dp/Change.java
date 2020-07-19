a30e4944cbb5c9a8dae4c6015fac8d9767c91e11
caogang <caogang_sx@qiyi.com>
2020-07-19 15:03:30 +0800

    518. 零钱兑换 II(DP)

--- src/com/leet/dp/Change.java
+++ src/com/leet/dp/Change.java
@@ -33,4 +33,41 @@ public class Change {
         }
         return sum;
     }
+
+    // dp
+    public int changeDP(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                // 注意
+                dp[i][j] = dp[i - 1][j];
+                if (j - coins[i - 1] >= 0) {
+                    dp[i][j] += dp[i][j - coins[i - 1]];
+                }
+            }
+        }
+
+        return dp[n][amount];
+    }
+
+    // dp空间优化
+    public int changeDP2(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+
+        return dp[amount];
+    }
 }
