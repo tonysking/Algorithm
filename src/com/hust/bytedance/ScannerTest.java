7321fd831d1d84047907e6429773ffdf21687fb8
caogang <caogang_sx@qiyi.com>
2020-08-23 17:04:29 +0800

    ScannerTest

--- src/com/hust/bytedance/ScannerTest.java
+++ src/com/hust/bytedance/ScannerTest.java
@@ -30,7 +30,17 @@ public class ScannerTest {
         // 读取一个整数---注意：nextInt按空格分开 多输出的整数会读取到后面的next.Int中
         int nextInt = sc.nextInt();
         System.out.println("读取一个整数："+nextInt);*/
+
         // 读取一个数组
+//        readIntArray();
+
+        // 读取二维字符数组
+        readCharArray();
+
+    }
+
+    public static void readIntArray() {
+        Scanner sc =  new Scanner(System.in);
         int n = sc.nextInt();
         int[] a = new int[n];
         for (int i = 0; i < n; i++) {
@@ -41,5 +51,36 @@ public class ScannerTest {
             System.out.print(a[i]+" ");
         }
         System.out.println();
+    }
+
+    /**
+     * 4
+     * CHIA
+     * CANT
+     * GRAC
+     * BBDE
+     */
+    public static void readCharArray() {
+        Scanner sc =  new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] a = new String[n];
+        char[][] chars = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.next();
+            for (int j = 0; j < n; j++) {
+                chars[i][j] = a[i].charAt(j);
+            }
+        }
+
+        printCharArray(chars);
+    }
+
+    public static void printCharArray(char[][] chars) {
+        for (char[] cs : chars) {
+            for (char c : cs) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
     }
 }
