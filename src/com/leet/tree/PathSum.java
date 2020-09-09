e6e4f71b26bed8d49f798f50fe8ae04ea2aaf83d
caogang <caogang_sx@qiyi.com>
2020-09-09 18:44:43 +0800

    剑指 Offer 34. 二叉树中和为某一值的路径

--- src/com/leet/tree/PathSum.java
+++ src/com/leet/tree/PathSum.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 /**
  * 113. 路径总和 II
+ * 剑指 Offer 34. 二叉树中和为某一值的路径
  */
 public class PathSum {
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
@@ -14,6 +15,7 @@ public class PathSum {
             return res;
         }
         pathSummer(root, lPath, res, 0, sum);
+        dfs(root, sum, new ArrayList<>());
         return res;
     }
 
@@ -32,5 +34,26 @@ public class PathSum {
         pathSummer(root.right, lPath, res, curSum, sum);
         // 注
         lPath.remove(lPath.size() - 1);
+    }
+
+    List<List<Integer>> res2 = new ArrayList<>();
+    public void dfs(TreeNode root, int sum, List<Integer> path) {
+        if(root == null) {
+            return;
+        }
+        path.add(root.val);
+        sum -= root.val;
+        if(sum == 0 && root.left == null && root.right == null) {
+            res2.add(new ArrayList<>(path));
+        }
+
+        dfs(root.left, sum, path);
+        dfs(root.right, sum, path);
+        path.remove(path.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        PathSum res = new PathSum();
+        res.pathSum(TreeNode.getTree(), 7);
     }
 }
